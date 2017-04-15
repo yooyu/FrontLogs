d19a770e4097a464858c4d625341c482108646ea
shenweiwei <shenweiwei9@163.com>
2017-04-15 23:43:30 +0800

    update by sww

---
+++ backend/parent/src/main/java/web/yooyu/parent/App.java
@@ -0,0 +1,13 @@
+package web.yooyu.parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
