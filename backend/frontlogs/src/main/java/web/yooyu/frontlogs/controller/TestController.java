333254623aa87ce9291ddd3d4db348a685436490
shenweiwei <shenweiwei9@163.com>
2017-04-22 01:04:48 +0800

    update by sww

--- backend/frontlogs/src/main/java/web/yooyu/frontlogs/controller/TestController.java
+++ backend/frontlogs/src/main/java/web/yooyu/frontlogs/controller/TestController.java
@@ -1,5 +1,26 @@
 package web.yooyu.frontlogs.controller;
 
-public class TestController {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * Hello world!
+ *
+ */
+@Controller
+@EnableAutoConfiguration
+public class TestController 
+{
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TestController.class, args);
+    }
 }
